0c900e856af82b00f0a60067260d557005978ab8
Valdemir <vpsilva@gmail.com>
2020-06-12 10:35:06 -0300

    Initial

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,36 +1,54 @@
 package Application;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import db.DB;
 
 public class Program {
 
 	public static void main(String[] args) {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Connection conn = null;
+		PreparedStatement st = null;
 
-		Connection _Conn = null;
-		Statement _St = null;
-		ResultSet _Rs = null;
-		
 		try {
-			_Conn = DB.getConnection();
-			_St = _Conn.createStatement();
+			conn = DB.getConnection();
+			st = conn.prepareStatement("Insert into Seller " + "(Name,Email,BirthDate,BaseSalary,DepartmentId)"
+					+ " Values " + "(?,?,?,?,?)",
+					Statement.RETURN_GENERATED_KEYS);
+
+			st.setString(1, "Carl Purple");
+			st.setString(2, "Carl@Gmail.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1955").getTime()));
+			st.setDouble(4, 3000.0);
+			st.setInt(5, 4);
+
+			int RowsAffected = st.executeUpdate();
 			
-			_Rs = _St.executeQuery("Select * From Department");			
-			while (_Rs.next()) {
-				System.out.println(_Rs.getInt("Id")+", "+_Rs.getString("Name"));
+			if(RowsAffected >0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while (rs.next()) {
+					int Id = rs.getInt(1);
+					System.out.println("Rows Affected:"+ RowsAffected + ", Id:"+Id);
+				}
+				
+			} else {
+				System.out.println("No Rows Affected:");
 			}
-		} 
-		catch (SQLException e) {
-				e.printStackTrace();
-		}
-		finally {
-			DB.closeResultSet(_Rs);
-			DB.closeStatement(_St);
-			DB.closeConnection();			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
 		}
 	}
 }
